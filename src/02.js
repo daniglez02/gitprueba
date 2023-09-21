443bb82b4832a444a270330b5bb5e1b78ef7e0bc
Osvaldo Gonzalez <2122300433@soy.utj.edu.mx>
2023-09-20 18:36:00 -0600

    Practica 2 continuacion

---
+++ src/02.js
@@ -0,0 +1,10 @@
+const nombre = 'Osvaldo';
+const apellido = 'Gonzalez';
+
+const nombreCompleto = nombre + '  ' + apellido;
+console.log(nombreCompleto);
+
+const nombreCompleto2 = ` Hola 
+
+mundo `;
+console.log(nombreCompleto2);
---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+const nombre = 'Osvaldo';
+const apellido = 'Gonzalez';
+
+let valorDado = 5;
+
+if( true ){
+    let valorDado =6;
+    console.log(valorDado);
+}
+
+console.log(nombre,apellido, valorDado);
